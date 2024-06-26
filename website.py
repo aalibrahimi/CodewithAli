fb6edc82cfa36c095c1303e4a85da2b58cf0382d
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2024-06-26 13:57:47 -0700

    two new pages and new website look

--- website.py
+++ website.py
@@ -1,18 +1,43 @@
-#To get changes do: git pull (for all branches)
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-#Stacking @app.route() will make it so the user can enter in the URL bar any name (that we wrote) and it'll take them to the same (part of the) website.
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-#Putting @app.route() seperate will make a new 'tab' on the website.
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/courses")
+def courses():
+    return render_template('courses.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/testimonials")
+def testimonials():
+    return render_template('testimonials.html')
+
+# @app.route("/blog")
+# def blog():
+#     return render_template('blog.html') 
+
+@app.route("/faq")
+def faq():
+    return render_template('faq.html')
+
+@app.route("/submit_contact", methods=["POST"])
+def submit_contact():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    # Here you would typically process the contact form submission
+    # For example, save it to a database or send an email
+    return f"Thank you for your message, {name}!"
 
 if __name__ == '__main__':
     app.run(debug=True)
